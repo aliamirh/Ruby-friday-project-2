85cc5d02dc6e97fab9c4eb94b0df2555ed77daac
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2019-12-26 13:44:48 -0800

    creats test

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -22,21 +22,40 @@ describe "Word" do
     end
   end
   describe('#==') do
-  it("is the same word if it has the same attributes as another word") do
-    word = Word.new("Apple", nil )
-    word2 = Word.new("Apple", nil)
-    expect(word).to(eq(word2))
+    it("is the same word if it has the same attributes as another word") do
+      word = Word.new("Apple", nil )
+      word2 = Word.new("Apple", nil)
+      expect(word).to(eq(word2))
+    end
   end
-end
-describe('.find') do
-  it("finds an word by id") do
-    word = Word.new("Grape", nil)
-    word.save()
-    wor2 = Word.new("Apple", nil)
-    word2.save()
-    expect(Word.find(word.id)).to(eq(word))
+  describe('.find') do
+    it("finds an word by id") do
+      word = Word.new("Grape", nil)
+      word.save()
+      word2 = Word.new("Apple", nil)
+      word2.save()
+      expect(Word.find(word.id)).to(eq(word))
+    end
   end
-end
+  describe('#update') do
+    it("updates an word by id") do
+      word = Word.new("Apple", nil)
+      word.save()
+      word.update("Grape")
+      expect(word.words).to(eq("Grape"))
+    end
+  end
+
+  describe('#delete') do
+    it("deletes an word by id") do
+      word = Word.new("Apple", nil)
+      word.save()
+      word2 = Word.new("Grape", nil)
+      word2.save()
+      word.delete()
+      expect(Word.all).to(eq([word2]))
+    end
+  end
 
 
 
