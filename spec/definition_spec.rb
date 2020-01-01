c60a6604f11d8a1f0cb99c0f03d986c0221ad71d
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2019-12-31 22:31:47 -0800

    add test

---
+++ spec/definition_spec.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+require 'definition'
+require 'word'
+require 'pry'
+
+describe '#Definition' do
+
+  before(:each) do
+    Word.clear()
+    Definition.clear()
+    @word = Word.new("Giant Steps", nil)
+    @word.save()
+  end
+end
