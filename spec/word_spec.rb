116aca97946d4fe8e4d3b291dbe37c3e83ddb61a
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2019-12-31 16:24:21 -0800

    ran into a lot of issues redid tests and lay out

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,18 +1,18 @@
 require 'rspec'
-require 'pry'
 require 'word'
 
-describe "Word" do
+
+describe "#Word"do
   before(:each) do
     Word.clear()
   end
+
   describe(".all") do
-    it("returns an empty array when there are no albums") do
-      expect(Word.all).to(eq([]))
+    it("Returns an empty array") do
+      expect(Word.all()).to(eq([]))
     end
   end
-
-  describe('#save') do
+  describe(".save") do
     it("saves a word") do
       word = Word.new("Apple", nil)
       word.save()
@@ -23,26 +23,38 @@ describe "Word" do
   end
   describe('#==') do
     it("is the same word if it has the same attributes as another word") do
-      word = Word.new("Apple", nil )
+      word = Word.new("Apple", nil)
       word2 = Word.new("Apple", nil)
       expect(word).to(eq(word2))
     end
   end
-  describe('.find') do
-    it("finds an word by id") do
-      word = Word.new("Grape", nil)
+  describe('.clear') do
+    it("clears all words") do
+      word = Word.new("Apple", nil)
       word.save()
-      word2 = Word.new("Apple", nil)
+      word2 = Word.new("Grape", nil)
+      word2.save()
+      Word.clear()
+      expect(Word.all).to(eq([]))
+    end
+  end
+
+  describe('.find') do
+    it("finds an words by id") do
+      word = Word.new("Apple", nil)
+      word.save()
+      word2 = Word.new("Grape", nil)
       word2.save()
       expect(Word.find(word.id)).to(eq(word))
     end
   end
+
   describe('#update') do
     it("updates an word by id") do
       word = Word.new("Apple", nil)
       word.save()
-      word.update("Grape")
-      expect(word.words).to(eq("Grape"))
+      word.update("Peach")
+      expect(word.name).to(eq("Peach"))
     end
   end
 
@@ -56,9 +68,4 @@ describe "Word" do
       expect(Word.all).to(eq([word2]))
     end
   end
-
-
-
-
-
 end
