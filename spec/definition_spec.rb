f3dd0cc671326ad265ddb0efa55391f6bc09b223
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2019-12-31 22:36:24 -0800

    add test

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -8,7 +8,16 @@ describe '#Definition' do
   before(:each) do
     Word.clear()
     Definition.clear()
-    @word = Word.new("Giant Steps", nil)
+    @word = Word.new("Apple", nil)
     @word.save()
   end
+  
+  describe('#==') do
+    it("is the same definition if it has the same attributes as another definition") do
+      definition = Definition.new("fruit", @word.id, nil)
+      definition2 = Definition.new("fruit", @word.id, nil)
+      expect(definition).to(eq(definition2))
+    end
+  end
+
 end
